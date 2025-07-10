00ea11aa302965982f194445c544c6ccf359d8a8
preetham lk <135232525+1234shadowtech@users.noreply.github.com>
2025-07-10 15:40:12 +0530

    hi

---
+++ a.py
@@ -0,0 +1,2 @@
+def SQr(x):
+    return x**0.5
--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-for i in range(0, 10):  # Loop from 0 to 9 (10 iterations). The variable `i` is unused in the loop.
-    print("hi")  # Print "hi" in each iteration.
+from a import SQr
+a=10
+print(SQr(a))
