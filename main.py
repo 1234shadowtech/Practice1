dcddda41fe850feaecad225c9e1bba4fcff7e640
preetham lk <135232525+1234shadowtech@users.noreply.github.com>
2025-07-10 13:08:46 +0530

    hi

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-```python
-def SQRT(x):  # Function to calculate the square root of a number
-    return x**0.5  # Returns the square root using exponentiation
-
-a = 10  # Variable to store the first number
-b = 20  # Variable to store the second number (currently unused)
-print(SQRT(a))  # Prints the square root of the value stored in 'a'
-```
+def SQR_ADD(x,y):
+    return (x**0.5)+y
+a=10
+b=20
+print(SQR_ADD(a,b))
