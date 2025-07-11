2daa205613ea5bad59e63aaa19f0fc5f5101556d
preetham lk <135232525+1234shadowtech@users.noreply.github.com>
2025-07-11 13:35:16 +0530

    hi

--- b.py
+++ b.py
@@ -1,3 +1,3 @@
 def water(x):  
-    m = x * x  # The variable `m` is unnecessary. You can directly return the result of the calculation.  
-    return m**0.5  # Consider renaming the function and variable to improve clarity.
+    m = x * x *x   
+    return m**0.5  
