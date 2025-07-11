89170a109451736c87c270e2e1fb223c418dff31
preetham lk <135232525+1234shadowtech@users.noreply.github.com>
2025-07-11 13:43:40 +0530

    hi

--- b.py
+++ b.py
@@ -1,3 +1,3 @@
-def water(x):  # Rename this function to something more descriptive, e.g., `calculate_square_root_of_square`.  
-    m = x * x  # This calculates the square of the input value.  
+def water(x):   
+    m = x * x *x  
     return m**0.5  # This calculates the square root of the squared value, effectively returning the original input value. Update the comment accordingly.
