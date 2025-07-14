08c47fc8820635ddcc651b663a632316b1b15852
preetham lk <135232525+1234shadowtech@users.noreply.github.com>
2025-07-14 16:23:10 +0530

    hi

--- b.py
+++ b.py
@@ -1,4 +1,3 @@
 def special(x):   
-    # The function calculates the sum of two square roots of x.
-    # However, the square root is calculated twice, which is redundant.
-    return (x**0.5) + (x**0.5)  # This can be optimized by calculating the square root once and reusing it.
+    
+    return (x**0.5) * (x**0.5)  
