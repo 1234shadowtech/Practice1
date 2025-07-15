1dd0b4423e58bc207c807d9022ec3877534f0ef8
preetham lk <135232525+1234shadowtech@users.noreply.github.com>
2025-07-15 12:48:09 +0530

    hi

--- second.py
+++ second.py
@@ -1,4 +1,3 @@
-from first import sq, sqr  # Importing two functions, sq and sqr, from the module 'first'. Ensure 'first' exists and is correctly implemented.
-
-def special(x):  # Defining a function 'special' that takes one argument 'x'.
-    return sq(x) + sqr(x)  # Calls the 'sq' and 'sqr' functions on 'x' and returns their sum. Ensure 'sq' and 'sqr' are implemented correctly.
+from first import sq, sqr  
+def special(x):  
+    return (sq(x) - sqr(x))*10  
