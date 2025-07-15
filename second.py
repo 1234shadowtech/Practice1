c182299832198e894a87764ec3a6616b5757499d
preetham lk <135232525+1234shadowtech@users.noreply.github.com>
2025-07-15 15:46:57 +0530

    hi

--- second.py
+++ second.py
@@ -1,6 +1,5 @@
-from first import sq, sqr  # Importing `sq` (square function) and `sqr` (square root function) from the `first` module.
-
+from first import sq, sqr  
 def special(x):  # Defining the `special` function, which calculates a specific mathematical operation on `x`.
     # Calculate the difference between the square of `x` (`sq(x)`) and the square root of `x` (`sqr(x)`).
     # Multiply the result by 2 and return it.
-    return (sq(x) - sqr(x)) * 2  # Returning the result of the mathematical operation.
+    return (sq(x) - sqr(x)) * 3 # Returning the result of the mathematical operation.
