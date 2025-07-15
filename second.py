1dbebd1caa1d67f12589e80e78d0646857946148
preetham lk <135232525+1234shadowtech@users.noreply.github.com>
2025-07-15 12:55:26 +0530

    hi

--- second.py
+++ second.py
@@ -1,4 +1,4 @@
-from first import sq, sqr  # Importing `sq` and `sqr` functions from the `first` module. Ensure these functions are defined and implemented correctly.
-
-def special(x):  # Defining the `special` function that takes one argument `x`.
-    return (sq(x) - sqr(x)) * 2  # Subtracting the result of `sqr(x)` from `sq(x)` and multiplying the result by 2. Ensure `sq` and `sqr` return compatible numeric types.
+from first import sq, sqr  
+def special(x):  
+    m=(sq(x) - sqr(x)) * 2
+    return (sq(x) - sqr(x)) * 2  
