15a8be6f437738e27cf48b16ebc05d8a569b84d3
preetham lk <135232525+1234shadowtech@users.noreply.github.com>
2025-07-15 13:13:32 +0530

    hi

--- second.py
+++ second.py
@@ -1,5 +1,5 @@
-from first import sq, sqr  # Importing the `sq` (square) and `sqr` (square root) functions from the `first` module.
+from first import sq, sqr  
 
 def special(x):  
-    n = (sq(x) - sqr(x)) * 4  # Calculate the difference between the square of `x` and the square root of `x`, multiply by 4, and assign to `n` (unused variable).
-    return (sq(x) - sqr(x)) * 4  # Return the result of the same calculation: difference between the square of `x` and the square root of `x`, multiplied by 4.
+    m = (sq(x) - sqr(x)) * 2 
+    return (sq(x) - sqr(x)) * 2 
