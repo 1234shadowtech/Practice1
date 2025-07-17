6b64cafb31a04fb99e930db588a1a3b06dc5ab20
preethamlk <lkpreethamgowda@gmail.com>
2025-07-18 00:01:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from help import special
+
 def sqr(x):  
     return x*x  # Returns the square of the input value `x`.
 
@@ -8,3 +10,4 @@ a=10  # Assigns the value 10 to the variable `a`.
 b=20
 print(mod(a))  # Prints the result of the `mod` function when called with `a` as the argument.
 print(sqr(b))
+print(special(a,b))
