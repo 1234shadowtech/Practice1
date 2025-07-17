d16589a46369ab270500dbd2fefef0ec989413f9
preethamlk <lkpreethamgowda@gmail.com>
2025-07-17 18:37:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ def mod(x):
     return x%2  # Returns the remainder when `x` is divided by 2.
 
 a=10  # Assigns the value 10 to the variable `a`.
-
+b=20
 print(mod(a))  # Prints the result of the `mod` function when called with `a` as the argument.
+print(sqr(b))
