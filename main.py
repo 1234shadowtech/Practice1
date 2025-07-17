52ff5d283715073ca087f24bad9c528b819cf1f8
preetham lk <135232525+1234shadowtech@users.noreply.github.com>
2025-07-18 00:32:56 +0530

    hi

--- main.py
+++
@@ -1,9 +0,0 @@
-def sqr(x):  
-    return x*x  # Returns the square of the input value `x`.
-
-def mod(x):  
-    return x%2  # Returns the remainder when `x` is divided by 2.
-
-a=10  # Assigns the value 10 to the variable `a`.
-
-print(mod(a))  # Prints the result of the `mod` function when called with `a` as the argument.
