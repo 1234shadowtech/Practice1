e4cc01085f66d8478c16f992015fc1d578202344
preethamlk <lkpreethamgowda@gmail.com>
2025-07-17 16:08:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from help import substract  # Importing the 'substract' function from the 'help' module
-
+from help2 import Sqr_mul
 from special import s  # Importing the 's' function from the 'special' module
 
 a = 10  # Assigning the value 10 to variable 'a'
@@ -7,3 +7,4 @@ b = 20  # Assigning the value 20 to variable 'b'
 
 print(s(a))  # Calling the 's' function with 'a' as an argument and printing the result
 print(substract(b, a))  # Calling the 'substract' function with 'b' and 'a' as arguments and printing the result
+print(Sqr_mul(a,b))
