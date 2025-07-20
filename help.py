3407dceff2e95517986101dbf92318956fcef58d
preetham lk <135232525+1234shadowtech@users.noreply.github.com>
2025-07-20 17:29:31 +0530

    hi

---
+++ help.py
@@ -0,0 +1,8 @@
+def sqr(x):
+    return x**0.5
+
+def mod(x):
+    return x%2
+
+def flo(x):
+    return x//2
---
+++ main.py
@@ -0,0 +1,7 @@
+from help import sqr,mod,flo
+
+a=10
+
+print(sqr(a))
+print(mod(a))
+print(flo(a))
