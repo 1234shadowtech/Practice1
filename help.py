71066af8e45f5701046941c0d99547930118ee9c
preetham lk <135232525+1234shadowtech@users.noreply.github.com>
2025-07-21 11:53:42 +0530

    hi

--- help.py
+++
@@ -1,8 +0,0 @@
-def sqr(x):
-    return x**0.5  # Calculates the square root of x
-
-def mod(x):
-    return x%2  # Returns the remainder when x is divided by 2
-
-def flo(x):
-    return x//2  # Performs floor division of x by 2
--- main.py
+++
@@ -1,7 +0,0 @@
-from help import sqr, mod, flo  # Importing three functions from the 'help' module: sqr, mod, and flo.
-
-a = 10  # Assigning the value 10 to the variable 'a'.
-
-print(sqr(a))  # Calling the 'sqr' function with 'a' and printing the result.
-print(mod(a))  # Calling the 'mod' function with 'a' and printing the result.
-print(flo(a))  # Calling the 'flo' function with 'a' and printing the result.
