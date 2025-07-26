72faf1d34f7ca889ccd21b6a59c3aa991273d2e1
preetham lk <135232525+1234shadowtech@users.noreply.github.com>
2025-07-27 02:01:42 +0530

    hi

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
-def sqr(x):  
-    # This function calculates the square root of x, not the square. Consider renaming to `sqrt`.  
-    return x**0.5  
+def sqr(x):
+    return x**0.5
 
-def fool(x):  
-    # This function performs integer division by 2. Consider renaming to something more descriptive like `halve_integer`.  
-    return x//2  
+de div(x):
+    return x//2
 
-# Syntax error: `de` should be `def`.  
-def mod(x):  
-    # This function calculates the modulus of x by 2.  
-    return x%2  
+a=10
 
-a = 10  # Variable `a` is defined correctly.  
+print(sqr(a))
 
-# Error: `x` is undefined. Replace `x` with `a` or define `x` before this line.  
-print(sqr(a))  
-
-# Correct usage of `fool` with variable `a`.  
-print(fool(a))  
-
-# Correct usage of `mod` with variable `a`.  
-print(mod(a))
+print(div(a))
