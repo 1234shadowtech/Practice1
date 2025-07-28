261cb81057847b51602217dcb35ce429bbf161d3
preetham lk <135232525+1234shadowtech@users.noreply.github.com>
2025-07-28 12:45:45 +0530

    hi

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-print("hi, welcome")  # Prints a welcome message.
-
-def Sq(x: int) -> int: 
-    # Returns the square of the input integer.
-    return x * x 
-
-def Sqr(x: int) -> float:  # Updated return type to float for accuracy.
-    # Returns the square root of the input integer.
-    return x**0.5
-
-a = 10  # Assigns the value 10 to variable 'a'.
-print(Sq(a))  # Prints the square of 'a'.
-print(Sqr(a))  # Prints the square root of 'a'.
+print("Hi, Welcome")
